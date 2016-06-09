70b99f957dd872c546927c311744e093c8a56907
moulin <yoshimura-takuya@kayac.com>
2016-06-09 14:46:10 +0900

    version up

--- cmd/gocal/gocal.go
+++ cmd/gocal/gocal.go
@@ -37,7 +37,7 @@ var (
 )
 
 func main() {
-	gocalCmd.Version("v0.0.1")
+	gocalCmd.Version("v0.0.2")
 	subcmd := kingpin.MustParse(gocalCmd.Parse(os.Args[1:]))
 
 	conf, err := gocal.LoadConfig(*confFile)
